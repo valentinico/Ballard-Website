39bc6dd24345f4dece4f6ab2db88e2536dba3f03
valentinico <valentinrosciano77@gmail.com>
2014-07-28 12:30:23 -0300

    Store Index
    index_store.html 95% done
    index_store.html mobile 50% done

--- js/ballard.js
+++ js/ballard.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 <!-- scrollr inicializacion si es desktop-->
-
+var currOption='collapseOne';currIconType='';
 var device = navigator.userAgent
 if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
  { 
@@ -65,10 +65,29 @@ else
 <!-- toggle bar -->
 
 $(document).ready(function () {
+    
   $('[data-toggle="offcanvas"]').click(function () {
 	$('.row-offcanvas').toggleClass('active')
   });
 });
+
+<!-- more and less -->
+
     
-    
+function changeIcon(cont) {
+    var iconType='＋skrollr';
+    $('.panel .panel-heading .more').html('＋');
+    if(currOption != cont){    
+        $('.panel .panel-heading .'+cont).html('－');
+        iconType='－';
+    }else if(currIconType==iconType){
+        if(currIconType == '＋'){
+            $('.panel .panel-heading .'+cont).html('－');iconType='－';
+        }else{
+            $('.panel .panel-heading .'+cont).html('＋');iconType='＋';
+        }
+    }
+    currOption = cont;
+    currIconType=iconType;
+}    
     
