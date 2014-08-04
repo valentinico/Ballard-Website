a176285d839fbb3ec44490a5ad1c117fcc4928cf
valentinico <valentinrosciano77@gmail.com>
2014-08-04 16:08:32 -0300

    New Icons
    New Icons and others changes.

--- js/store.js
+++ js/store.js
@@ -1,54 +1,4 @@
-// JavaScript Document
-
-<!-- scrollr inicializacion si es desktop-->
 var currOption='collapseOne';currIconType='';
-var device = navigator.userAgent
-if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
- { 
-	$(function(){<!-- Si es un mobile, solo activa un desplazamiento suave-->
-
-		 $('a[href*=#]').click(function() {
-	
-		 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-			 && location.hostname == this.hostname) {
-	
-				 var $target = $(this.hash);
-	
-				 $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-	
-				 if ($target.length) {
-	
-					 var targetOffset = $target.offset().top;
-	
-					 $('html,body').animate({scrollTop: targetOffset}, 1000);
-	
-					 return false;
-	
-				}
-	
-		   }
-	
-	   });
-	
-	});
- 
-
-}
-else
-{	<!-- Si es desktop, activa skrollr con todas sus funciones-->
-	setTimeout(function() {
-		var s = skrollr.init();
-		
-		skrollr.menu.init(s, {
-		animate: true,
-		duration: 1000,
-		easing: 'sqrt'
-		})
-		
-	}, 500);
-	
-}
-
 
 <!-- Preloader -->
 
@@ -110,7 +60,7 @@ function toggleNav() {
 
     
 function changeIcon(cont) {
-    var iconType='＋skrollr';
+    var iconType='＋';
     $('.panel .panel-heading .more').html('＋');
     if(currOption != cont){    
         $('.panel .panel-heading .'+cont).html('－');
@@ -124,5 +74,10 @@ function changeIcon(cont) {
     }
     currOption = cont;
     currIconType=iconType;
+}    
+    
+    
+function changeNavIcon() {
+    $( "#nav_arrow h3" ).toggle();
 }    
     
